f6d6808483889391c7182769b10d60a4cde69773
raphael2211 <raphealejiofor419@gmail.com>
2026-06-17 15:28:00 +0100

    fix: remove datasources from PrismaClient for v7

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,11 +5,7 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
+    log: ['query'],
   });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
